ba42d5b36c3c26f457780199d5e795a38028ec1d
Holbrook Wong <wanghaikuo@gmail.com>
2012-11-13 11:54:18 +0800

    +) add models

--- envadmin/devsupport/models.py
+++ envadmin/devsupport/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from subject.models import Person
 # Create your models here.
+class Project(models.Model):
+	name = models.CharField(max_length=255)
+	manager = models.ForeignKey("Person", blank=True, null=True, related_name="manager")
+	
--- envadmin/resources/models.py
+++ envadmin/resources/models.py
@@ -1,7 +1,10 @@
+# coding=utf-8
 from django.db import models
 from mptt.models import MPTTModel
 
 # Create your models here.
+class Subdomain(models.Model):
+	name = models.CharField(max_length=100)
 class ResourceType(models.Model):
 	name = models.CharField(max_length=100)
 
@@ -11,7 +14,15 @@ class  ResourceAssociationType(MPTTModel):
 		
 
 class Resource(MPTTModel):
+	SUBDOMAIN_CHOICES = (  
+        (u'DEV', u'开发环境'),  
+        (u'UAT', u'UAT环境'),
+        (u'RUN', u'生产环境'),  
+    )   
+
 	type = models.ForeignKey('ResourceType')
 	parent = models.ForeignKey("self", blank=True, null=True, related_name="children")
+
+	Subdomain = models.CharField(max_length=3, choices=SUBDOMAIN_CHOICES)
 
 
