723214d61efb1f19b66be47aa210967e6e1842a3
holbrook <wanghaikuo@gmail.com>
2012-11-16 10:24:32 +0800

    refactor

--- ea/docs/js graph/老牌，功能全，收费draw2d_0.9.17/no_namespace/single/connector_direct/ToolUndo.js
+++
@@ -1,9 +0,0 @@
-ToolUndo=function(_4018){
-Button.call(this,_4018);
-};
-ToolUndo.prototype=new Button;
-ToolUndo.prototype.type="ToolUndo";
-ToolUndo.prototype.execute=function(){
-this.palette.workflow.getCommandStack().undo();
-ToolGeneric.prototype.execute.call(this);
-};
